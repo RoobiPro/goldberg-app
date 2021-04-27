9e27fe3327955669faa8bea3f8d64ea901dc67b9
Schahin Rouhanizadeh <s.rouhanizadeh@freenet.de>
2021-04-27 13:39:36 +0200

    Debug

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
+// Route::get('/', function () {
+//     // return view('welcome');
+//     return view('vuedashboard');
+// })->where('any', '.*');
+
+Route::get('/{any}', function () {
     return view('vuedashboard');
-})->where('any', '.*');
+})->where('any', '.*')->middleware(['auth'])->name('dashboard');
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
-//
-// require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
 
 
-Route::get('/test', function () {
-  return view('vuedashboard');
-});
+// Route::get('/test', function () {
+//   return view('vuedashboard');
+// });
 
 // ->where('any', '.*');
