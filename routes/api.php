83dbe99d04e0fcdfaf60b8cab4f7fb1c40a1ce2b
Robinson Guerra <robinson.guerra@protonmail.com>
2021-04-28 12:51:06 +0800

    sactum + user resources

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+
+Route::get('/users', function () {
+    return UserResource::collection(User::paginate(10));
+});
+
+Route::get('/user/{id}', function ($id) {
+    return new UserResource(User::findOrFail($id));
 });
