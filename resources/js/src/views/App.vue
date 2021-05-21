3ca6d964317a980f9cb24f1ca13885a5800daf94
Schahin Rouhanizadeh <s.rouhanizadeh@freenet.de>
2021-05-21 18:43:39 +0200

    Added some Transitions from Login to App

--- resources/js/src/views/App.vue
+++ resources/js/src/views/App.vue
@@ -1,5 +1,11 @@
 <template>
+
+  <transition name="fade">
+
     <router-view />
+
+  </transition>
+
 </template>
 
 <script>
@@ -7,3 +13,14 @@
     name: 'App'
   }
 </script>
+
+<style>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+</style>
