4c3a357e712bbcb26088a9abcd393a2f8b539a72
Schahin Rouhanizadeh <s.rouhanizadeh@freenet.de>
2021-05-31 05:31:09 +0200

    Chillaui

--- app/Http/Controllers/FileImportController.php
+++ app/Http/Controllers/FileImportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Models\Campaigns\Drilling;
 
 
@@ -10,40 +11,53 @@ class FileImportController extends Controller
 {
     //
     public function index(Request $req){
+
+      // Check CSV Filetype!
+      // $fileType = $req->filetype;
+      // if (!$req->filetype){
+      //   return response()->json([ "success" => false, "msg" => 'Upload Data has no Filetype!'], 200);
+      // }
+
+      // Check CSV File
       if($req->hasFile('csvfile')){
         $filenameWithExt = $req->file('csvfile')->getClientOriginalName();
         $extension = $req->file('csvfile')->getClientOriginalExtension();
 
         $row=0;
         $handle = fopen($req->file('csvfile'), "r");
-            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-              if($row==0){
-                $row++;
-                continue;
-              }
-              Drilling::create([
-                  'project_id' => 1,
-                  'drilling_code' => $data[1],
-                  'type' =>  $data[9],
-                  'utm_x' => $data[3],
-                  'utm_y' => $data[4],
-                  'utm_z' => $data[5],
-                  'start_date' => now(),
-                  'end_date' => now(),
-                  'dip' => $data[7],
-                  'length' => $data[8],
-                  'azimuth' => $data[6]
-              ]);
-              return $data[1];
-                $num = count($data);
-                return $num;
-                // echo "<p> $num Felder in Zeile $row: <br /></p>\n";
-                $row++;
-                foreach(range(1, 100, $num) as $data) {
-                  echo $data;
-              }
+
+        while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+          if($row==0){
+              $row++;
+              continue;
             }
-        // return response()->json([ "success" => true, "namewithext" => $filenameWithExt, 'extension' => $data[0] ], 200);
+
+          Drilling::create([
+              'project_id' => 1,
+              'drilling_code' => $data[1],
+              'type' =>  $data[9],
+              'utm_x' => $data[3],
+              'utm_y' => $data[4],
+              'utm_z' => $data[5],
+              'start_date' => now(),
+              'end_date' => now(),
+              'dip' => $data[7],
+              'length' => $data[8],
+              'azimuth' => $data[6]
+            ]);
+          return $data[1];
+          $num = count($data);
+          return $num;
+          // echo "<p> $num Felder in Zeile $row: <br /></p>\n";
+          $row++;
+
+        }
+
+      }
+
+        // return response()->json([ "success" => true, "msg" => 'CSV Uploaded Succesfully'], 200);
+
+        // Example
         // $row=0;
         // if (($handle = fopen("test.csv", "r")) !== FALSE) {
         //     while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
@@ -56,8 +70,7 @@ class FileImportController extends Controller
         //     }
         //     fclose($handle);
         // }
-
         // return response()->json([ "success" => true, "namewithext" => $filenameWithExt, 'extension' => $extension ], 200);
-      }
+
     }
 }
