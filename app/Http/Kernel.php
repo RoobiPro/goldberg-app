44f8b41fa4766c92dae30d10eef09633c483d4f3
RoobiPro <robinson.guerra90@googlemail.com>
2024-07-17 00:52:46 +0800

    Update middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,16 +40,36 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
+    
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Session\Middleware\StartSession::class, // Add this line
             // \App\Http\Middleware\CheckSessionStatus::class,
-
         ],
     ];
+    
+    // protected $middlewareGroups = [
+    //     'web' => [
+    //         \App\Http\Middleware\EncryptCookies::class,
+    //         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+    //         \Illuminate\Session\Middleware\StartSession::class,
+    //         // \Illuminate\Session\Middleware\AuthenticateSession::class,
+    //         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    //         \App\Http\Middleware\VerifyCsrfToken::class,
+    //         \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    //     ],
+
+    //     'api' => [
+    //         \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            
+    //         'throttle:api',
+    //         \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    //         // \App\Http\Middleware\CheckSessionStatus::class,
+
+    //     ],
+    // ];
 
 
 
